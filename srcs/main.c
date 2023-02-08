dd622311437395e060f73b778f151ef579066f42
dsas <dsas@student.42wolfsburg.de>
2023-02-08 20:30:16 +0100

    few

--- srcs/main.c
+++ srcs/main.c
@@ -21,13 +21,27 @@ int	main(void)
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
+	int color = 0x00FF0000;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
 	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	my_mlx_pixel_put(&img, 960, 540, 0x00FF0000);
+	int n_x;
+	int n_y = 100;
+	int n_finish = 1200;
+	while (n_y < 1000)
+	{
+		n_x = 500;
+		while (n_x < n_finish)
+		{
+			my_mlx_pixel_put(&img, n_x, n_y, color);
+			color+=5;
+			n_x++;
+		}
+		n_finish--;
+		n_y++;
+	}
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
-
